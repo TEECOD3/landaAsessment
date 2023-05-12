e66d780117b9bd377e18429c7f8de522deeca57e
david temitope <temi236topemicheal@gmail.com>
2023-05-12 16:33:34 +0100

    vercel production fix and adjustments

--- app/LearningPage/ShortcoursesPage/components/PorgressBar.tsx
+++ app/LearningPage/ShortcoursesPage/components/PorgressBar.tsx
@@ -10,7 +10,7 @@ const Progressbar: FC<ProgressbarProps> = (props: ProgressbarProps) => {
   return (
     <div className="flex flex-col md:flex-row  md:gap-3 items-center justify-start w-[4rem] md:w-[27rem] md:mt-4">
       <div className="w-[7rem] md:w-[17rem] flex items-center justify-center">
-        <div className="w-full h-[5px] rounded-lg bg-gray/80  ">
+        <div className="w-full h-[5px] rounded-lg bg-gray/60  ">
           <div
             className={`w-${progress} w-[80%]  h-full rounded-lg bg-blue-300 transition-all delay-150 duration-200 ease-in`}
           ></div>
